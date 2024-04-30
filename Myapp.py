6951440aba5ca6afda02b7e5d10fe226f9c9e6f9
AneriPatel28 <anerigirishbhai.patel@gwu.edu>
2024-04-30 17:00:34 -0400

    Update Myapp.py
    Updated App

--- Myapp.py
+++ Myapp.py
@@ -1,14 +1,83 @@
+
+""""
+
+Importing Required Modules
+
+"""
+
+
 import streamlit as st
 from melodygenerator import MelodyGenerator
-from music21 import environment, stream, note, pitch, tempo
 import torch
-import subprocess
 import os
+from music21 import environment, stream, note, pitch, converter, tempo
+import subprocess
 
-# Setup page configuration and title
+
+
+
 SEQUENCE_LENGTH = 64
 
 
+def apply_dark_theme():
+    st.set_page_config(page_title="Melody Generator", layout="wide")
+
+    # Define dark theme CSS with targeted adjustments for space reduction
+    dark_theme_css = """
+    <style>
+    html {
+        --primary: #000000;
+        --background-color: #002b36;
+        --main-text-color: #ffffff;
+        --secondary-text-color: #ffffff;  /* White color */
+        --main-color: #073642;
+        --color: #ffffff;
+    }
+    body {
+        margin: 0;
+        padding: 0;
+        font-family: 'IBM Plex Sans', sans-serif;
+    }
+    .stApp {
+        padding-top: 0 !important;  /* Remove padding at the top of the app */
+    }
+    header {
+        display: none;  /* Hides the Streamlit header completely */
+    }
+    .block-container {
+        padding: 1rem 1rem;  /* Adjust overall padding inside the app */
+    }
+    .stButton>button {
+        color: var(--main-text-color);
+        background-color: var(--primary);
+    }
+    .stTextInput>div>div>input, .stTextArea>textarea, .stTextInput .st-bd, .stTextArea .st-bd {
+        background-color: var(--main-color);
+        color: var(--secondary-text-color);
+    }
+    .st-bj {
+        background-color: var(--main-color);
+    }
+    /* Additional styles for subheaders */
+    .stSubheader {
+        background-color: var(--main-color)
+        color: var(--secondary-text-color);
+    }
+    /* Additional styles to reduce top spacing */
+    .css-1adrfps {
+        padding-top: 0px;
+    }
+    .stMarkdown {
+        background-color: var(--main-text-color);
+        color: var(--secondary-text-color);
+    }
+    </style>
+    """
+
+
+    st.markdown(dark_theme_css, unsafe_allow_html=True)
+
+apply_dark_theme()
 # Set device for model computations
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -17,27 +86,12 @@ env = environment.Environment()
 env['musescoreDirectPNGPath'] = '/usr/bin/musescore'
 
 # Define global settings
-input_size, hidden_size, output_size = 38, 128, 38
-model_path = "model.pth"
-sound_font_path = '/path/to/your/soundfont.sf2'  # Adjust this to your actual sound font path
-# Set title
-st.set_page_config(
-    page_title="Melody Generator",
-    page_icon="🎵",
-    layout="wide",
-    initial_sidebar_state="expanded",
-    menu_items={
-        'Get Help': 'https://www.example.com/help',
-        'Report a bug': "https://www.example.com/bug",
-        'About': "# This is a wide-mode music generation app. Enjoy creating melodies!"
-    }
-)
-
-st.title("Welcome to the AI Music Generator!")
+input_size, hidden_size, output_size = 38, 128, 38# Adjust this to your actual sound font path
 
 # Set title
- # Ensure this path is correct
 
+
+st.title("Welcome to the Melody Generator! 🎵")
 
 def midi_number_to_note_name(midi_number):
     """Converts MIDI number to a music21 note name, returns None if it's a rest placeholder."""
@@ -82,16 +136,6 @@ def text_to_score(text_melody, bpm=120):
 
     return s
 
-
-
-
-# Then in your Streamlit app
-import os
-
-
-
-import subprocess
-
 def convert_midi_to_wav(midi_file_path, output_file_path, sound_font):
     command = [
         'fluidsynth',
@@ -113,19 +157,6 @@ def convert_midi_to_wav(midi_file_path, output_file_path, sound_font):
 # Define path to your sound font file
 sound_font_path = '/home/ubuntu/generating-melodies-with-rnn-lstm/9 - Converting Generated Melodies to MIDI//soundfont.sf2'
 
-
-
-
-
-
-# Button and functionality to generate melody, image, and play sound
-# Initialize session state variables if not already set
-
-
-
-
-from music21 import environment, stream, note, pitch, converter, tempo
-import subprocess
 # Function to delete files if they exist
 env = environment.Environment()
 env['musescoreDirectPNGPath'] = '/usr/bin/musescore'
@@ -167,18 +198,32 @@ def save_score_image_with_xvfb(score, file_name="score.png"):
     subprocess.run(['xvfb-run', '-a', 'musescore', '-o', file_name, temp_musicxml], check=True)
     delete_file_if_exists(temp_musicxml)
 
-# Handle melody generation and display
-def handle_melody_generation(seed_text):
+
+model_path_lstm = "model.pth"
+model_path_gru = "model.pth"
+model_path_lstm_attention = "model.pth"
+
+
+
+# Define function to handle melody generation
+def handle_melody_generation(seed_text, model_choice):
     if seed_text:
+        if model_choice == 'LSTM':
+            model_path = model_path_lstm
+        elif model_choice == 'GRU':
+            model_path = model_path_gru
+        else:
+            model_path = model_path_lstm_attention
+
         melody_generator = MelodyGenerator(input_size, hidden_size, output_size, model_path=model_path)
         melody_generator.model.to(device)
         melody = melody_generator.generate_melody(seed_text, 500, SEQUENCE_LENGTH, 0.3)
         generated_melody_str = ' '.join([str(m) for m in melody])
-
+        st.subheader("Melody")
         # Save and display melody
         melody_generator.save_melody(melody, file_name="generated_melody.mid")
-
         convert_midi_to_wav("generated_melody.mid", "generated_melody.wav", sound_font_path)
+
         if os.path.exists("generated_melody.wav"):
             with open("generated_melody.wav", "rb") as wav_file:
                 st.audio(wav_file.read(), format='audio/wav')
@@ -186,16 +231,26 @@ def handle_melody_generation(seed_text):
             st.error("Failed to convert MIDI to WAV.")
 
         # Display and download MIDI
-        st.text_area("Generated Melody", generated_melody_str, height=150)
-        st.download_button("Download MIDI", data=open("generated_melody.mid", "rb").read(), file_name="generated_melody.mid", mime="audio/midi")
+        st.subheader("Generated Melody")
+        st.text_area("",generated_melody_str, height=150)
+        st.download_button("Download MIDI", data=open("generated_melody.mid", "rb").read(),
+                           file_name="generated_melody.mid", mime="audio/midi")
 
         # Generate score image
+
         score = text_to_score(generated_melody_str)
         save_score_image_with_xvfb(score)
+        st.subheader("Music Sheet")
         st.image("score-1.png", caption="Generated Sheet Music")
 
 
-# UI for seed melody input
-seed_text = st.text_input("Enter a seed melody (notes separated by spaces):", value="67 _ 67 _ 67 _ _ 65 64 _ 64 _ 64 _ _")
+# Streamlit UI
+
+
+seed_text = st.text_input("Enter a seed melody (notes separated by spaces):",
+                          value="67 _ 67 _ 67 _ _ 65 64 _ 64 _ 64 _ _")
+model_choice = st.radio("Choose the model type:", ('LSTM', 'GRU', 'LSTM_attention'),horizontal=True)
+
+
 if st.button('Generate Melody'):
-    handle_melody_generation(seed_text)
+    handle_melody_generation(seed_text, model_choice)
